4872e0e5ebb30d1cb9632f3e683f617e0ebecef4
barnapaul <paul.barna@emanuel.ro>
2018-05-05 23:46:59 +0300

    monster attack done

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+
+var config = {
+    context: __dirname + '/src',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader :'postcss-loader',
+                options:{
+                    plugins:function(){
+                        return[
+                            require('precss'),
+                            require('autoprefixer')
+                        
+                        ];
+                    }
+                }
+            },{
+                
+                
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }],
+        rules : [{
+            test : /\.css$/,
+            use : ['style-loader','css-loader']
+        }]
+    },
+    entry: ['./js/main.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'main.js',
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+};
+
+module.exports = config;
